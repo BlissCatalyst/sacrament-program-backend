8fdd52763aa5e1303dbe965ede4aae68118ed420
Logan Hufstetler <logan.hufstetler@gmail.com>
2020-09-22 22:12:49 -0600

    Basic Express Boiler

--- api/server.js
+++ api/server.js
@@ -1,5 +1,13 @@
 const express = require("express");
+
 const configuredMiddleware = require("./configuredMiddleware.js");
+
 const server = express();
+
+configuredMiddleware(server);
+
+server.get("/", (req, res) => {
+    res.send("Server is Running!");
+});
 
 module.exports = server;
